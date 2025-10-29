adcb4a988f1aae8edd648b68dfa3db3d1968599c
germanno@proton.me <longhi-beck.germanno@fronius.com>
2025-10-29 16:09:06 +0100

    Update e2e tests

--- system-test/tests/e2e-tests/test_ui_dashboard.py
+++ system-test/tests/e2e-tests/test_ui_dashboard.py
@@ -1,13 +1,14 @@
 from playwright.sync_api import expect
 
 
-def test_dashboard_greeting(logged_in_page):
+def test_dashboard(logged_in_page):
     page = logged_in_page
-    expect(page.get_by_text("Welcome, addisonw")).to_be_visible()
+    expect(page.get_by_text("Your Reservations")).to_be_visible()
+    expect(page.get_by_text("Your Cars")).to_be_visible()
 
 
 def test_view_cars_redirection(logged_in_page):
     page = logged_in_page
     print(page.content())
     page.get_by_role("link", name="View All").click()
-    expect(page).to_have_title("Your Cars")
+    expect(page.get_by_role("link", name="Add Car")).to_be_visible()
