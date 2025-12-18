bd8739095ba1ec3ab8189b42728c77794e2e9424
germanno@proton.me <germanno@proton.me>
2025-12-18 15:46:05 +0100

    Remove renamed folder

--- system-test/tests/e2e-tests/test_ui_dashboard.py
+++
@@ -1,7 +0,0 @@
-from playwright.sync_api import expect
-
-
-def test_view_cars_redirection(logged_in_page):
-    page = logged_in_page
-    page.get_by_role("button", name="View All").click()
-    expect(page.get_by_role("button", name="Add Car", exact=True)).to_be_visible()
