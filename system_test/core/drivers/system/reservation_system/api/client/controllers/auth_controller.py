9623e6e9f6853cd47bdbd8cdcca268930056b084
Germanno Longhi Beck <germanno@proton.me>
2026-06-04 18:26:50 +0200

    Fix type issues

--- system_test/core/drivers/system/reservation_system/api/client/controllers/auth_controller.py
+++ system_test/core/drivers/system/reservation_system/api/client/controllers/auth_controller.py
@@ -1,5 +1,7 @@
-from system_test.core.drivers.commons.result import Result
 from system_test.core.drivers.commons.clients.http_test_client import HttpTestClient
+from system_test.core.drivers.commons.clients.http_test_utils import HttpTestUtils
+from system_test.core.drivers.commons.clients.typed_response import TypedResponse
+from system_test.core.drivers.commons.result import Result
 from system_test.core.drivers.system.commons.dtos.auth_request import (
     LoginRequest,
 )
@@ -7,7 +9,6 @@ from system_test.core.drivers.system.commons.dtos.auth_response import (
     CurrentUserResponse,
     LoginResponse,
 )
-from system_test.core.drivers.commons.clients.http_test_utils import HttpTestUtils
 
 
 class AuthController:
@@ -17,9 +18,11 @@ class AuthController:
         self._http_client = http_client
 
     def login(self, request: LoginRequest) -> Result[LoginResponse]:
-        response = self._http_client.post(f"{self.AUTH_PATH}/login", request)
+        response: TypedResponse = self._http_client.post(
+            f"{self.AUTH_PATH}/login", request
+        )
         return HttpTestUtils.get_ok_result_or_failure(response)
 
     def get_current_user(self) -> Result[CurrentUserResponse]:
-        response = self._http_client.get(f"{self.AUTH_PATH}/me")
+        response: TypedResponse = self._http_client.get(f"{self.AUTH_PATH}/me")
         return HttpTestUtils.get_ok_result_or_failure(response)
